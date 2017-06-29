e20c6aa884aa9b9d7fbab6471f31c73ab52ee77c
pokerSeven <cxlhjwow@163.com>
2017-06-29 23:43:17 +0800

    test1

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,11 @@
 from __future__ import unicode_literals
 from models import  Article
 from django.contrib import admin
-admin.site.register(Article)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title','content','pub_time')
+    list_filter = ('pub_time',)
+    pass
+
+admin.site.register(Article,ArticleAdmin)
 # Register your models here.
