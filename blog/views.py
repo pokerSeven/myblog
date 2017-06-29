d9cccee87def7450d75edccaa5ba30cec2632d2c
pokerSeven <cxlhjwow@163.com>
2017-06-29 18:22:02 +0800

    test1

--- blog/views.py
+++ blog/views.py
@@ -7,5 +7,9 @@ from . import models
 # Create your views here.
 
 def index(request):
-    article = models.Article.objects.get(pk=1)
-    return render(request, "blog/index.html", {'artice':article})
+    articles = models.Article.objects.all()
+    return render(request, "blog/index.html", {'articles':articles})
+
+def article_page(request,article_id):
+    article = models.Article.objects.get(pk=article_id)
+    return render(request,'',{'article':article})
