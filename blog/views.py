c57479d1408a78eddfee375d5cbda200b172d868
pokerSeven <cxlhjwow@163.com>
2017-06-29 16:43:28 +0800

    first modify

---
+++ blog/views.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+# Create your views here.
+
+def index(request):
+    article = models.Article.objects.get(pk=1)
+    return render(request, "blog/index.html", {'artice':article})
